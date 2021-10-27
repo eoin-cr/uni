52bd423395ac3a51d2fbdfc1d61596e23e0ca053
Eoin <eoin@arch-legion-5.home>
2021-10-27 14:21:45 +0100

    Racket sorting algorithm

---
+++ Funct-Prog/sorter.rkt
@@ -0,0 +1,19 @@
+#lang racket
+(define sorter
+    (lambda (x)
+      (cond ((null? x) '())
+            ((null? (cdr x)) x)
+            ((< (car x) (second x)) (cons (car x) (sorter (cdr x))))
+            (#t (cons (second x) (sorter (cons (car x) (cdr(cdr x))))))
+      )
+    )
+)
+
+(define sort
+    (lambda (x)
+      (cond ((null? x) '())
+            ((equal? x (sorter x)) x)
+            (#t (sort (sorter x)))
+      )
+    )
+)
